38b541f447866b10b506ce1b240deefdafc77bc4
Naimur Nafiz <nurpustcse@gmail.com>
2020-01-08 23:29:33 +0600

    restored previous

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,5 @@
 <?php include 'asset/include/header.php'; ?>
 	<div class="container col-md-4">
-	
 		<a href="logout.php"><button class="btn btn-warning btn-block" >Logout</button></a>
 	</div>
 </body>
